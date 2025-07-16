bfd78d447ef20e619b91480eb900152d32bd6328
Karen Kamal <karenkamal24@gmail.com>
2025-07-17 00:08:51 +0300

    BannerController

--- app/Http/Controllers/User/BannerController.php
+++ app/Http/Controllers/User/BannerController.php
@@ -8,16 +8,17 @@ use App\Models\Banner;
 
 class BannerController extends Controller
 {
-    public function index()
-    {
-        $banners = Banner::where('is_active', true)->get(['id', 'image']);
+public function index()
+{
+    $banners = Banner::all(['id', 'image']);
 
-        return response()->json([
-            'status' => true,
-            'data' => $banners->map(fn ($banner) => [
-                'id' => $banner->id,
-                'image_url' => asset('storage/' . $banner->image),
-            ]),
-        ]);
-    }
+    return response()->json([
+        'status' => true,
+        'data' => $banners->map(fn ($banner) => [
+            'id' => $banner->id,
+            'image_url' => asset('storage/' . $banner->image),
+        ]),
+    ]);
+}
+
 }
