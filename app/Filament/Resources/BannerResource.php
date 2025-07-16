1c70b138730bd301b7a96bbea6efa4ced09c2ebd
Karen Kamal <karenkamal24@gmail.com>
2025-07-16 23:49:14 +0300

    resource fliment banner

---
+++ app/Filament/Resources/BannerResource.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BannerResource\Pages;
+use App\Filament\Resources\BannerResource\RelationManagers;
+use App\Models\Banner;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+
+
+class BannerResource extends Resource
+{
+    protected static ?string $model = Banner::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            FileUpload::make('image')
+                ->image()
+                ->directory('phramacy')
+                ->imagePreviewHeight('150')
+                ->columnSpanFull()
+                ->required(),
+
+            Toggle::make('is_active')->label('Active'),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            ImageColumn::make('image')
+                ->getStateUsing(fn ($record) => asset('storage/' . $record->image))
+                ->size(50)
+                ->circular()
+                ->label('Image'),
+
+            IconColumn::make('is_active')
+                ->boolean()
+                ->label('Active'),
+        ])->defaultSort('id', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBanners::route('/'),
+            'create' => Pages\CreateBanner::route('/create'),
+            'edit' => Pages\EditBanner::route('/{record}/edit'),
+        ];
+    }
+}
